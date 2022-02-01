159d5e77437d6c5d21733afa8aa3b08cff5d30ad
yunzae <81746373+yunzae@users.noreply.github.com>
2022-02-02 00:47:55 +0900

    Update 큰수의법칙.py

--- 그리디/큰수의법칙.py
+++ 그리디/큰수의법칙.py
@@ -1,4 +1,4 @@
-"""
+#내코드
 import sys
 n,m,k = map(int,sys.stdin.readline().rstrip().split(" "))
 mylist = []
@@ -9,7 +9,7 @@ mylist.sort()
 result = (m/(k+1))*(int(mylist[-1])*k+int(mylist[-2]))+(m%(k+1))*int(mylist[-1])
 print(int(result))
 
-"""
+#예시코드
 import sys
 n,m,k = map(int,sys.stdin.readline().split(" "))
 data = list(map(int,input().split()))
